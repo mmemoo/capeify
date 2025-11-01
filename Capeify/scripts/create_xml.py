d807ff5c83399dafaa649bb33f4f5a656bcced7e
mmemoo <mehmet.culcu2010@gmail.com>
2025-11-01 17:58:54 +0300

    create create_cursor func

---
+++ Capeify/scripts/create_xml.py
@@ -0,0 +1,34 @@
+from lxml import etree
+
+
+def create_cursor(
+    framecount, frameduration, hotspotx, hotspoty, pointshigh, pointswide, data
+):
+    template = etree.parse("Capeify/templates/cursor_template.cape")
+    template = template.getroot()
+
+    dict_ = template[1]
+
+    framecount_ = dict_[1]
+    framecount_.text = str(framecount)
+
+    frameduration_ = dict_[3]
+    frameduration_.text = str(frameduration)
+
+    hotspotx_ = dict_[5]
+    hotspotx_.text = str(hotspotx)
+
+    hotspoty_ = dict_[7]
+    hotspoty_.text = str(hotspoty)
+
+    pointshigh_ = dict_[9]
+    pointshigh_.text = str(pointshigh)
+
+    pointswide_ = dict_[11]
+    pointswide_.text = str(pointswide)
+
+    array = dict_[13]
+    data_ = array[0]
+    data_.text = str(data)
+
+    return template
