a0211f696dbf5adb0c5a46bb61297978186c7865
mmemoo <mehmet.culcu2010@gmail.com>
2025-11-01 18:30:37 +0300

    created read_inf func

---
+++ Capeify/scripts/read_inf.py
@@ -0,0 +1,20 @@
+def read_strings(path):
+    with open(path, "r") as f:
+        strings = {}
+        in_strings = False
+        for line in f.readlines():
+            line = line.strip()
+
+            if in_strings and line.strip() != "":
+                key, val = line.split("=")
+
+                strings[key] = val[1:-1]
+
+            if line != "" and len(line) >= 2:
+                if line[0] == "[" and line[-1] == "]" and in_strings:
+                    break
+
+            if line == "[Strings]":
+                in_strings = True
+
+    return strings
