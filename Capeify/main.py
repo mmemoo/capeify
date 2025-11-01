2a0c92de4e53637c197b07d838924f444ab4ca86
mmemoo <mehmet.culcu2010@gmail.com>
2025-11-01 18:59:29 +0300

    created windowsn cur name to mac os cur name dict

---
+++ Capeify/main.py
@@ -0,0 +1,21 @@
+import .scripts.ani as ani
+import .scripts.cur as cur
+import .scripts.create_xml as create_xml
+import .scripts.read_inf as read_inf
+
+win2mac_cur = {
+    "pointer" : "Arrow",
+    "busy" : "Wait",
+    "cross" : "Crosshair",
+    "wait" : "Busy",
+    "text" : "IBeam",
+    "hand" : "Open",
+    "unavailable" : "Forbidden",
+    "vert" : "Window E-W",
+    "horz" : "Window N-S",
+    "dgn1" : "Window NW-SE",
+    "dgn2" : "Window NE-SW",
+    "move" : "Resize Square",
+    "alternate" : "Link",
+    "link" : "Pointing"
+               }
