3607626bcea9f9636f58a57e18b63a6242776693
mmemoo <mehmet.culcu2010@gmail.com>
2025-11-02 19:48:33 +0300

    created get_size func for ani

---
+++ Capeify/scripts/ani/get_size.py
@@ -0,0 +1,13 @@
+import struct
+from ani_file import ani_file
+
+
+def get_size(file):
+    ani = ani_file.open(file, "r")
+
+    curs = ani.getframesdata()
+    data = curs[0]
+
+    width, height = struct.unpack(">II", png_data[16:24])
+
+    return width, height
