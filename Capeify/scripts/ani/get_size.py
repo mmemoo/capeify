351d6b9bca31ead032e8a8ca2f5123f133c0a151
mmemoo <mehmet.culcu2010@gmail.com>
2025-11-10 18:18:51 +0300

    fixed var name mismatch between data and png_data

--- Capeify/scripts/ani/get_size.py
+++ Capeify/scripts/ani/get_size.py
@@ -6,7 +6,7 @@ def get_size(file):
     ani = ani_file.open(file, "r")
 
     curs = ani.getframesdata()
-    data = curs[0]
+    png_data = curs[0]
 
     width, height = struct.unpack(">II", png_data[16:24])
 
