2aa4be8f8249b425bbe8564c731fed96ac85df56
mmemoo <mehmet.culcu2010@gmail.com>
2025-11-15 15:30:41 +0300

    created read_defaultInstall func

--- Capeify/scripts/read_inf.py
+++ Capeify/scripts/read_inf.py
@@ -19,3 +19,28 @@ def read_strings(path):
                 in_strings = True
 
     return strings
+
+
+def read_defaultInstall(path):
+    dict_ = {}
+    in_defaultInstall = False
+    with open(path, "r") as f:
+        for line in f.readlines():
+            if line != "" and len(line) >= 2:
+                if (
+                    line.strip()[0] == "["
+                    and line.strip()[-1] == "]"
+                    and in_defaultInstall
+                ):
+                    break
+
+            if in_defaultInstall and line.strip() != "":
+                key, val = line.strip().split("=")
+                key, val = key.strip().replace(",", ""), val.strip().replace(",", "")
+
+                dict_[key] = val
+
+            if "[DefaultInstall" in line.strip():
+                in_defaultInstall = True
+
+    return dict_
