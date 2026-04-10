c9251dbc1748051312c9dbb6fca83c77a35fdf97
mmemoo <mehmet.culcu2010@gmail.com>
2026-04-11 00:02:16 +0300

    fix reg type identification,fix some flaws in reg parsing and also add some docstrings to functions (for some old ones and for all of the new ones). also universally fixes #5

--- Capeify/main.py
+++ Capeify/main.py
@@ -17,15 +17,15 @@ import argparse
 from time import time
 
 win2mac_cur = {
-    0: ["com.apple.coregraphics.Arrow"],
-    1: ["com.apple.cursor.40"],
-    2: ["com.apple.cursor.4"],
-    3: ["com.apple.coregraphics.Wait"],
-    4: ["com.apple.cursor.7", "com.apple.cursor.8", "com.apple.cursor.41"],
-    5: ["com.apple.coregraphics.IBeam"],
-    6: None,
-    7: ["com.apple.cursor.3"],
-    8: [
+    "Arrow": ["com.apple.coregraphics.Arrow"],
+    "Help": ["com.apple.cursor.40"],
+    "AppStarting": ["com.apple.cursor.4"],
+    "Wait": ["com.apple.coregraphics.Wait"],
+    "Crosshair": ["com.apple.cursor.7", "com.apple.cursor.8", "com.apple.cursor.41"],
+    "IBeam": ["com.apple.coregraphics.IBeam"],
+    "NWPen": None,
+    "No": ["com.apple.cursor.3"],
+    "SizeNS": [
         "com.apple.cursor.32",
         "com.apple.cursor.21",
         "com.apple.cursor.22",
@@ -33,7 +33,7 @@ win2mac_cur = {
         "com.apple.cursor.31",
         "com.apple.cursor.36",
     ],
-    9: [
+    "SizeWE": [
         "com.apple.cursor.28",
         "com.apple.cursor.17",
         "com.apple.cursor.18",
@@ -41,13 +41,13 @@ win2mac_cur = {
         "com.apple.cursor.27",
         "com.apple.cursor.38",
     ],
-    10: ["com.apple.cursor.34", "com.apple.cursor.33", "com.apple.cursor.35"],
-    11: ["com.apple.cursor.30", "com.apple.cursor.29", "com.apple.cursor.37"],
-    12: ["com.apple.cursor.39"],
-    13: ["com.apple.cursor.2"],
-    14: ["com.apple.cursor.13"],
-    15: None,
-    16: None,
+    "SizeNWSE": ["com.apple.cursor.34", "com.apple.cursor.33", "com.apple.cursor.35"],
+    "SizeNESW": ["com.apple.cursor.30", "com.apple.cursor.29", "com.apple.cursor.37"],
+    "SizeAll": ["com.apple.cursor.39"],
+    "UpArrow": ["com.apple.cursor.2"],
+    "Hand": ["com.apple.cursor.13"],
+    "Pin": None,
+    "Person": None,
 }
 
 
@@ -59,15 +59,22 @@ def convert(args):
     strings = read_inf.read_strings(inf_file_path)
     strings = {key.lower(): val for key, val in strings.items()}
 
-    reg_sect = read_inf.read_defaultInstall(inf_file_path)["AddReg"]
-    reg_sect = reg_sect.split(",")[0]
-    reg = read_inf.read_reg(inf_file_path, reg_sect)
+    reg_header = read_inf.read_defaultInstall(inf_file_path)["AddReg"][0]
+    reg_header = reg_header.split(",")[0]
+    raw_reg = read_inf.read_reg(inf_file_path, reg_header)
+    reg_sep = read_inf.determine_sep(raw_reg)
+    parsed_reg = read_inf.parse_reg(raw_reg, reg_sep)
 
     cursors = []
-    for idx, win_cur in enumerate(reg):
-        if win2mac_cur[idx]:
-            path = f"{args.path}/{strings[win_cur.lower()]}"
-            ext = strings[win_cur.lower()][-3:]
+    for win_cur_identifier, win_cur in parsed_reg.items():
+        if win2mac_cur[win_cur_identifier]:
+            win_cur_file = (
+                strings[win_cur.lower()[1:-1]]
+                if win_cur[0] == "%" and win_cur[-1] == "%"
+                else win_cur
+            )
+            path = f"{args.path}/{win_cur_file}"
+            ext = win_cur_file[-3:]
             if ext == "cur":
                 data = c_convert2png.convert_cur2png(path)
 
@@ -77,7 +84,7 @@ def convert(args):
                 hs_x, hs_y = c_get_hotspot.get_hotspot(path)
                 w, h = c_get_size.get_size(data)
 
-                for cur_name in win2mac_cur[idx]:
+                for cur_name in win2mac_cur[win_cur_identifier]:
                     cursors.append(
                         create_xml.create_cursor(
                             cur_name,
@@ -106,7 +113,7 @@ def convert(args):
                 frame_dur = a_get_frame_duration.get_frame_duration(path)
                 frame_dur = (frame_dur * real_frame_count) / lowered_frame_count
 
-                for cur_name in win2mac_cur[idx]:
+                for cur_name in win2mac_cur[win_cur_identifier]:
                     cursors.append(
                         create_xml.create_cursor(
                             cur_name,
@@ -120,7 +127,7 @@ def convert(args):
                         )
                     )
 
-        print(f'CAPEIFY $$ Cursor "{win_cur}" done.')
+            print(f"CAPEIFY $$ Cursor {win_cur_file} done.")
 
     cur_pack_name = args.path.split("/")[-1]
 
@@ -129,7 +136,7 @@ def convert(args):
     )
     cape.write(args.out, pretty_print=True)
 
-    print(f"CAPEIFY $$ Conversion done! Time elapsed : {time() - start} seconds.")
+    print(f"CAPEIFY $$ Conversion done! Time elapsed : {(time() - start):.4f} seconds.")
 
 
 def main():
--- Capeify/scripts/read_inf.py
+++ Capeify/scripts/read_inf.py
@@ -40,8 +40,7 @@ def read_defaultInstall(path):
 
             if in_defaultInstall and line.strip() != "":
                 key, val = line.strip().split("=")
-                key, val = key.strip().replace(",", ""), val.strip().replace(",", "")
-
+                key, val = key.strip(), val.strip().split(",")
                 dict_[key] = val
 
             if "[DefaultInstall" in line.strip():
@@ -50,19 +49,100 @@ def read_defaultInstall(path):
     return dict_
 
 
-def read_reg(path, reg_sect):
+def read_reg(path: str, reg_header: str) -> str:
+    """
+    get raw string from given reg header
+    """
     in_reg = False
+    reg_raw = ""
     with open(path, "r") as f:
         for line in f.readlines():
-            if in_reg and line.strip() != "":
-                read_csv = csv.reader([line.strip()])
-                read_csv = next(read_csv)
-                reg_str = read_csv[-1]
-
-                curs = reg_str.split(",")
-                curs = [cur.split("\\")[-1][1:-1] for cur in curs]
-
-                return curs
-
-            if line.strip() == f"[{reg_sect}]":
+            if line.strip() == "":
+                continue
+            elif line.strip() == f"[{reg_header}]":
                 in_reg = True
+            elif line.strip()[0] == "[" and in_reg:
+                return reg_raw
+            elif in_reg:
+                reg_raw += line
+
+
+idx_2_win_cur_identifier = {
+    0: "Arrow",
+    1: "Help",
+    2: "AppStarting",
+    3: "Wait",
+    4: "Crosshair",
+    5: "IBeam",
+    6: "NWPen",
+    7: "No",
+    8: "SizeNS",
+    9: "SizeWE",
+    10: "SizeNWSE",
+    11: "SizeNESW",
+    12: "SizeAll",
+    13: "UpArrow",
+    14: "Hand",
+    15: "Pin",
+    16: "Person",
+}
+
+
+def parse_reg(reg_raw: str, sep: str) -> dict[str, str]:
+    """
+    parse a raw reg string to get the cursors
+
+    returns a dict with the structure win_cursor_identifier : cursor file name/variable reference
+    """
+    if sep == ",":
+        """
+        case where reg_raw is something like:
+        HKCU,"Control Panel\\Cursors\\Schemes","%SCHEME_NAME%",,"%10%\\%CUR_DIR%\\%Arrow%,%10%\\%CUR_DIR%\\%Help%,%10%\\%CUR_DIR%\\%AppStarting%...
+        """
+        reg_parsed = reg_raw.split(sep)
+        reg_parsed = [elem for elem in reg_parsed if elem]
+        cursors = []
+
+        for elem in reg_parsed[3:]:
+            elem = elem.split("\\")[-1].strip()
+            elem = elem[:-1] if elem.endswith('"') else elem
+
+            # Not removing the %'s so i can check if the string is a variable reference or not in main.py
+            cursors.append(elem)
+
+        cursors = {idx_2_win_cur_identifier[i]: cur for i, cur in enumerate(cursors)}
+
+        return cursors
+    elif sep == "\n":
+        """
+        case where reg_raw is something like:
+        HKCU,"Control Panel\\Cursors",,0x00020000,"%SCHEME_NAME%"
+        HKCU,"Control Panel\\Cursors",AppStarting,0x00020000,"%10%\\%CUR_DIR%\\%working%"
+        HKCU,"Control Panel\\Cursors",Arrow,0x00020000,"%10%\\%CUR_DIR%\\%pointer%"
+        HKCU,"Control Panel\\Cursors",crosshair,0x00020000,"%10%\\%CUR_DIR%\\%precision%"
+        ...
+        """
+        reg_parsed = reg_raw.split(sep)
+        reg_parsed = [elem for elem in reg_parsed if elem]
+        cursors = {}
+
+        for elem in reg_parsed[1:-1]:
+            elem = elem.split(",")
+
+            win_cur_identifier = elem[2]
+            cur = elem[4].split("\\")[2]
+            cur = cur[:-1] if cur.endswith('"') else cur
+
+            cursors[win_cur_identifier] = cur
+
+        return cursors
+
+
+def determine_sep(reg: str) -> str:
+    """
+    determine the seperator in the reg
+
+    returns the seperator with the type "str"
+    """
+
+    return "\n" if "\n" in reg.strip() else ","
